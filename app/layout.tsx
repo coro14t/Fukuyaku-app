91d5581b8d08971a6aafeb67a9a87b96ddcc3a59
coro14t <coro2103@yahoo.co.jp>
2026-04-01 16:17:21 +0900

    change site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "俺emini Works",
+  title: "oremini Works",
   description: "個人開発アプリとブログのサイト",
 };
 
